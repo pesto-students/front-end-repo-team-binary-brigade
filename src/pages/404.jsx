2b9f3770012d41f73f71782ca2eff0e1bf5c516f
Dillip Kumar Sahu <sahudillipkumar466@gmail.com>
2023-08-07 10:55:03 +0530

    not found page redirect update

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,10 +2,21 @@ import { Button, Result } from "antd";
 import { Helmet } from "react-helmet";
 import { useNavigate } from "react-router-dom";
 import appConfig from "../app/config/app.config";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const PageNotFound = () => {
     const navigate = useNavigate();
-    
+    const { data } = useSelector(state => state.authReducer);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => navigate(data ? `/feed?$${data.user.college_id}` : '/login'), 2000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [data, navigate])
+
     return (
         <>
             <Helmet>
@@ -15,7 +26,7 @@ const PageNotFound = () => {
                 status="404"
                 title="404"
                 subTitle="Sorry, the page you visited does not exist."
-                extra={<Button type="primary" onClick={() => navigate('/')}>Back Home</Button>}
+                extra={<Button type="primary" onClick={() => navigate(data ? `/feed?$${data.user.college_id}` : '/login')}>Back Home</Button>}
             />
         </>
     )
